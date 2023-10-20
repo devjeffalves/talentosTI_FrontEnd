14cf27b2edf1487569966555d1b99aad699aaabf
devjeffalves <jeff-alves@outlook.com>
2023-10-19 21:55:06 -0300

    Estilizando Dark mode

---
+++ script.js
@@ -0,0 +1,8 @@
+let botao = document.getElementById("botao-alterar-tema");
+let body = document.querySelector("body");
+
+botao.addEventListener("click" , () => {
+body.classList.toggle('dark')});
+
+
+
