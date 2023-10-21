8f3a26d41ba35091f672e638211f8732a1c5df09
devjeffalves <jeff-alves@outlook.com>
2023-10-20 21:48:31 -0300

    adicionando menu

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-let botao = document.getElementById("botao-alterar-tema");
-let body = document.querySelector("body");
+let light = document.getElementById("botao-alterar-tema");
+let dark = document.getElementById("botao-alterar-tema-dark");
+let head = document.querySelector('#head')
+let body = document.querySelector('body')
+let block = document.querySelector('#block')
+let table = document.querySelector('table')
+// let p = document.querySelectorAll('p')
+// let p = document.querySelectorAll('p')
 
-botao.addEventListener("click" , () => {
-body.classList.toggle('dark')});
+dark.addEventListener("click", () => {
+    
+    body.classList.toggle('dark');
+    head.classList.toggle('dark');
+    block.classList.toggle('dark');
+    table.classList.toggle('dark');
+
+    // p.forEach((element) => {
+    //     element.classList.toggle('dark')
+    // });
+})
+
 
 
 
